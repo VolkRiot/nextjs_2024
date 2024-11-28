1215e49f86bc56a8b02a5c9fe496b8abe2416360
Mikhail Metrikin <metrikin@gmail.com>
2024-11-27 17:34:05 -0800

    more tutorial completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,13 +34,14 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
