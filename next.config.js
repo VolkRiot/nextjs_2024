bddd7fb00b287c3c56a5e085f6fbb698ac205c54
Mikhail Metrikin <metrikin@gmail.com>
2024-11-27 18:08:53 -0800

    one more try

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    missingSuspenseWithCSRBailout: false,
     experimental: {
         ppr: 'incremental',
-        missingSuspenseWithCSRBailout: false
       },
 };
 
